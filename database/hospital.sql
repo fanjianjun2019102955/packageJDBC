46219f3e550ede744160672197b85fc4aaa50ad7
fanjianjun2019102955 <38968595+fanjianjun2019102955@users.noreply.github.com>
2020-05-08 11:16:13 +0800

    数据库

--- database/hospital.sql
+++ database/hospital.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50513
 File Encoding         : 65001
 
-Date: 2020-05-04 22:42:41
+Date: 2020-05-08 11:15:32
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,20 +20,20 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `doctor`;
 CREATE TABLE `doctor` (
-  `userID` int(4) unsigned zerofill NOT NULL AUTO_INCREMENT,
+  `userID` int(10) unsigned zerofill NOT NULL AUTO_INCREMENT,
   `username` varchar(20) DEFAULT NULL,
   `password` varchar(50) DEFAULT '123456',
-  `sex` int(10) unsigned DEFAULT NULL COMMENT '0：男，1：女',
+  `sex` mediumint(10) unsigned DEFAULT NULL COMMENT '0：男，1：女',
   PRIMARY KEY (`userID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of doctor
 -- ----------------------------
-INSERT INTO `doctor` VALUES ('0001', 'lisi', '123456', '0');
-INSERT INTO `doctor` VALUES ('0002', 'zhangsan', '123456', '0');
-INSERT INTO `doctor` VALUES ('0003', 'duqie', '123456', '1');
-INSERT INTO `doctor` VALUES ('0004', 'wangwu', '123456', '0');
+INSERT INTO `doctor` VALUES ('0000000001', 'lisi', '123456', '0');
+INSERT INTO `doctor` VALUES ('0000000002', 'zhangsan', '123456', '0');
+INSERT INTO `doctor` VALUES ('0000000003', 'duqie', '123456', '1');
+INSERT INTO `doctor` VALUES ('0000000004', 'wangwu', '123456', '0');
 
 -- ----------------------------
 -- Function structure for `addTwoNum`
